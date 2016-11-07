6207bc06d2f8d5e955951e7161f85963e21727d1
KerryAlsace <kerrydougherty@gmail.com>
2016-11-07 17:14:37 -0500

    get form to work without ajax first

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,22 @@ class CommentsController < ApplicationController
     # render :json => @comments
   end
 
+  def create
+    @comment = @post.comments.build(comments_params)
+    if @comment.save
+      redirect_to @post
+    else
+      render 'posts/show'
+    end
+  end
+
   private
     def set_post
       @post = Post.find(params[:post_id])
+    end
+
+    def comments_params
+      params.require(:comment).permit(:content)
     end
 end
 
